a3525214ab76b62d565c5c36210d49967e355ef7
Max Kottong <max.kottong@gmail.com>
2022-10-16 14:33:58 -0400

    Added about page

---
+++ src/components/About/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+    return (
+        <section className="my-5">
+            <h1 id="about">About Me</h1>
+            <p>Lorem ipsum dolor nuash tyreth Lorem ipsum dolor nuash tyreth Lorem ipsum dolor nuash tyreth Lorem ipsum dolor nuash tyreth Lorem ipsum dolor nuash tyreth Lorem ipsum dolor nuash tyreth</p>
+        </section>
+    );
+}
+
+export default About;
