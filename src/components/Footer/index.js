7b4039523768289c836efd674df0e80d5ae9c4bc
Max Kottong <max.kottong@gmail.com>
2022-10-19 21:58:20 -0400

    Stackoverflow img

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import linkedIn from '../../assets/small/logos/LI-In-Bug.png'
 import gitHub from '../../assets/small/logos/GitHub-Mark-64px.png'
+import stackOverflow from '../../assets/small/logos/stackOverflow.png'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../assets/css/footer.css';
 
@@ -9,6 +10,7 @@ const Footer = () => {
         <div className="footer">
             <a href="https://www.linkedin.com/in/max-kottong-027778213/" target="_blank"><img src={linkedIn} width="75" height="64" alt="LinkedIn logo" /></a>
             <a href="https://github.com/MaxKottong" target="_blank"><img src={gitHub} alt="GitHub logo" /></a>
+            <a href="https://stackoverflow.com/users/19222944/max-kottong" target="_blank"><img src={stackOverflow} width="64" alt="Stackoverflow logo" /></a>
         </div>
     )
 }
